760fc5c144bc9dca77e0e79c832e6957f2dc3bf5
matiasmicenmacher <matamicen@gmail.com>
2019-04-24 19:24:47 -0300

    habilita el cross origin para que pueda ser invocada la API desde localhost

--- restDatabase/src/main/java/com/example/restDatabase/controller/ContactController.java
+++ restDatabase/src/main/java/com/example/restDatabase/controller/ContactController.java
@@ -8,6 +8,7 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -103,7 +104,7 @@ public class ContactController {
 	}
 	
 	
-	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping
 	public Contact create(@RequestBody Contact contact){
 	    return daocontact.save(contact);
