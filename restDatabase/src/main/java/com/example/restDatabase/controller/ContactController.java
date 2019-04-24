8efdcc1175468f50a9194e9d09ca19c52d068be6
matiasmicenmacher <matamicen@gmail.com>
2019-04-24 14:18:49 -0300

    POST con HEADER - Response con Object JSON custom

--- restDatabase/src/main/java/com/example/restDatabase/controller/ContactController.java
+++ restDatabase/src/main/java/com/example/restDatabase/controller/ContactController.java
@@ -4,7 +4,9 @@ package com.example.restDatabase.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.restDatabase.model.Contact;
 import com.example.restDatabase.repository.ContactRepository;
 
+
+
 @RestController
 @RequestMapping({"/contacts"})
 public class ContactController {
@@ -26,11 +30,10 @@ public class ContactController {
 	@Autowired
 	ContactRepository daocontact;
 	
-	List<Contact> contactFound;
 	
 	
 	@GetMapping
-	public List<Contact> findAll(){
+	public Iterable<Contact> findAll(){
 		System.out.println("paso por findAll");
 	  return daocontact.findAll();
 	}
@@ -39,13 +42,67 @@ public class ContactController {
 	public ResponseEntity<Contact> findById(@PathVariable long id){
 		System.out.println("paso por /id");
 		
+	Contact con = daocontact.findById(id).orElse(null);
+
 	
-	  return daocontact.findById(id)
-	          .map(record -> ResponseEntity.ok().body(record))
-	          .orElse(ResponseEntity.notFound().build());
+
+	if (con!=null) {
+	 System.out.println("encontro:"+con.getName() + con.getEmail());
+	
+	
+	return ResponseEntity.ok().body(con);
+	}else return ResponseEntity.notFound().build();
+		
+	
+//	  return daocontact.findById(id)
+//	          .map(record -> ResponseEntity.ok().body(record))
+//	          .orElse(ResponseEntity.notFound().build());
 		
 	
 	}
+	// public ResponseEntity<List<Object>> findByName(@PathVariable String name){
+	
+	@GetMapping(path = {"/name/{name}"})
+	public ResponseEntity<Object> findByName(@PathVariable String name){
+		System.out.println("paso por /name");
+		
+	List<Contact> contactsFound = daocontact.findByName(name);
+
+	if (contactsFound.size()>0) {	
+		//System.out.println("encontro:"+con.getName() + con.getEmail());
+		 for(Contact con: contactsFound)
+	     {
+			 System.out.println("name:"+con.getName() + con.getEmail());
+	     }
+	     
+		 JSONObject obj = new JSONObject();
+		 
+
+	      obj.put("error", 0);
+	      obj.put("results", contactsFound);
+	      
+	      
+	      
+
+	return ResponseEntity.ok().body(obj.toString());
+	}else {
+		JSONObject obj = new JSONObject();
+		 
+	   
+		obj.put("error", 1);
+		obj.put("description", "No data found");
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(obj.toString());
+	}
+		
+		
+		//return ResponseEntity.notFound().build();
+		
+	
+	
+	}
+	
+	
 	
 	@PostMapping
 	public Contact create(@RequestBody Contact contact){
--- restDatabase/src/main/java/com/example/restDatabase/repository/ContactRepository.java
+++ restDatabase/src/main/java/com/example/restDatabase/repository/ContactRepository.java
@@ -1,9 +1,14 @@
 package com.example.restDatabase.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.example.restDatabase.model.Contact;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
-
+	
+	
+	public List<Contact> findByName(String nombre);
 }
